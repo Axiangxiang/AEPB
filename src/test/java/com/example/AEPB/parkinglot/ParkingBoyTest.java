5d095357151f4340662e94c129dd10442a19bbb2
YouJiaming <jiaming.you@thoughtworks.com>
2021-10-19 13:04:17 +0800

    [axiang&jiaming] #homework-3 test:add  test.

--- src/test/java/com/example/AEPB/parkinglot/ParkingBoyTest.java
+++ src/test/java/com/example/AEPB/parkinglot/ParkingBoyTest.java
@@ -21,4 +21,25 @@ public class ParkingBoyTest {
         assertNotNull(parkingResult.getData());
     }
 
+    @Test
+    void should_park_success_when_parking_boy_park_car_given_parkinglot1_has_no_position_and_parkinglot2_has_position() {
+        ArrayList<ParkingLot> parkingLots = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            parkingLots.add(new ParkingLot());
+        }
+        ParkingLot parkingLot1 = parkingLots.get(0);
+        for (int i = 0; i < 50; i++) {
+            parkingLot1.park(new Car());
+        }
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLots);
+        ParkingResult parkingResult = parkingBoy.park(new Car());
+        ParkingLot parkingLot2 = parkingLots.get(1);
+        ParkingTicket parkingTicket = parkingResult.getData();
+
+        assertEquals("success", parkingResult.getStatus());
+        assertEquals(parkingLot2, parkingTicket.getParkingLot());
+
+
+    }
+
 }
